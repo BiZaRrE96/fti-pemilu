f9c52af76da6bb375735287a52c3aafcd20bcbbe
BiZaRrE96 <91012945+BiZaRrE96@users.noreply.github.com>
2025-03-05 14:09:21 +0700

    included alternative for card display

---
+++ src/renderer/src/components/Check_circle.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+// Courtesy of https://codepen.io/xristouz/pen/dybGXdd
+
+
+
+</script>
+
+<template>
+    <div class="container">
+        <svg id="Layer_2" class="on" viewBox="0 0 84 84"><circle class="cls-1" cx="42" cy="42" r="41"/></svg>
+        
+        <svg id="Layer_1" class="on" viewBox="0 0 24 19">
+        <polyline class="cls-2" points="2.09 9.56 8.3 15.77 22.19 1.87"/>
+        </svg>
+    
+    </div>
+</template>
+
+<style lang="css" scoped>
+
+.cls-1{fill:none;stroke:#229959;stroke-miterlimit:10;stroke-width:2px;}
+
+.cls-2{fill:none;stroke:#229959;stroke-miterlimit:10;stroke-width:1.5px;}
+
+body {
+  text-align: center;
+  background: #fff;
+}
+
+svg {
+  position: absolute;
+  display: block;
+}
+
+.container {
+  display: inline-block;
+  position: relative;
+  width: 92px;
+  height: 92px;
+   
+  #Layer_1 {
+    width: 42px;
+    left: 21px;
+    top: 26px;
+    stroke-dasharray: 30;
+    stroke-dashoffset: 30;
+
+    &.on {
+      animation: check .5s;
+      animation-delay: 1.2s;
+      -webkit-animation-fill-mode: forwards;
+      animation-fill-mode: forwards;
+    }
+  }
+
+  #Layer_2 {
+    width: 84px;
+    stroke-dasharray: 258;
+    stroke-dashoffset: 258;
+    transform: rotate(270deg);
+    
+    &.on {
+      animation: circle 1.4s;
+      -webkit-animation-fill-mode: forwards;
+      animation-fill-mode: forwards;
+    }
+  }
+}
+
+@keyframes circle {
+  0% {
+    stroke-dashoffset: 258;
+    transform: rotate(90deg);
+  }
+  100% {
+    stroke-dashoffset: 0;
+  }
+}
+
+@keyframes check {
+  0% {
+    stroke-dashoffset: 30;
+  }
+  100% {
+    stroke-dashoffset: 0;
+  }
+}
+
+</style>
---
+++ src/renderer/src/components/Panel.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+    //Example : <Panel @closed="() => {console.log('Closed bruh')}"/>
+
+    interface prop {
+        panelname? : string;
+        showexit? : boolean;
+    }
+
+    const props = withDefaults(defineProps<prop>(),{
+        panelname : "Info",
+        showexit : true
+    })
+
+    const emit = defineEmits(['closed'])
+
+    const opening = ref(true);
+    const closing = ref(false);
+
+    function flip() {
+        opening.value = false;
+        setTimeout(() => {
+            closing.value = true;
+        }, 10);
+    }
+
+    function handleAnimationEnd() {
+    if (closing.value) {
+        emit('closed')
+    }
+}
+
+</script>
+
+<template>
+    <div class="lepanel" :class="{open : opening, close : closing}" @animationend="handleAnimationEnd">
+        <span class="title">
+            <!-- The thing at top of panels; titlebars -->
+             {{ props.panelname }}
+             <button v-if="props.showexit" @click="() => {flip()}">< Return</button>
+        </span>
+        <span>
+            <slot>
+                Text here bub
+            </slot>
+            <!-- Content -->
+        </span>
+    </div>
+</template>
+
+<style lang="css" scoped>
+    .lepanel {
+        position: fixed;
+        display: flex;
+        flex-direction: column;
+        left: 20dvw;
+        top: 20dvh;
+        width: 60dvw;
+        height: 60dvh;
+        background-color: cornsilk;
+        opacity: 75%;
+        color: black;
+        /* Target
+        width: 60dvw;
+        height: 60dvh;
+        */
+
+        border: 2.5%;
+        border-radius: 25px;
+
+        span {
+            display: flex;
+            flex-grow: 1;
+            padding: 2.5% 2.5%;
+        }
+        .title {
+            display: flex;
+            flex-grow: 0;
+            justify-content: space-between;
+            padding: 1% 5%;
+        }
+    }
+
+    .lepanel.open {
+        animation: openpanel 0.25s ease-out;
+        animation-direction: normal;
+        animation-fill-mode: both;
+    }
+
+    .lepanel.close {
+        animation: openpanel 0.25s ease-in;
+        animation-direction: reverse;
+        animation-fill-mode: both;
+    }
+
+    @keyframes openpanel {
+    0% {
+        left: 50dvw;
+        top: 50dvh;
+        width: 0;
+        height: 0;
+        opacity: 0;
+        animation-timing-function: ease-out;
+    }
+    50% {
+        left: 30dvw;
+        top: 40dvh;
+        width: 40dvw;
+        height: 20dvh;
+    }
+    100% {
+        left: 20dvw;
+        top: 20dvh;
+        width: 60dvw;
+        height: 60dvh;
+        opacity: 1;
+        animation-timing-function: ease-in;
+    }
+}
+</style>
